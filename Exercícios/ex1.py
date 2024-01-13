86442a9d1d3dbd1055311d39a004f22e4dccab9e
Ian Pereira Simão <ianpereira2004vital@gmail.com>
2024-01-13 17:46:56 -0300

    Update ex1.py

--- Exercícios/ex1.py
+++ Exercícios/ex1.py
@@ -1,7 +1,21 @@
 #tarefa: selecione todos os números impares de 1 a 100, e então conte quantos números tem.
-contagem = 0
-for numero in range(1, 100):
-    if numero % 2 != 0:
-        print(numero)
+#adicional: faça o usuário selecionar entre ímpar ou par e a faixa numérica, não deixe que o programa deixe de rodar.
+def inicio():
+    f1 = int(input(f"digite o primeiro número da faixa desejada: "))
+    f2 = int(input(f"digite o último número da faixa desejada: "))
+    ip = input(f"par ou ímpar? ")
+    contagem = 0
+    if ip == "impar":
+        for numero in range((f1), (f2)):
+            if numero % 2 != 0:
+                print(numero)
         contagem += 1
-print(f"existem {contagem} números ímpares nesta faixa de números.")
+    print(f"existem {contagem} números ímpares nesta faixa de números.")
+
+    if ip == "par":
+      for numero in range((f1), (f2)):
+         if numero % 2 == 0:
+            print(numero)
+            contagem += 1
+    print(f"existem {contagem} números pares nesta faixa de números.")
+inicio()
